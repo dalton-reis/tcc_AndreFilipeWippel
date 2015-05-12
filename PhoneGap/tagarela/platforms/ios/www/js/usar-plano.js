d3fc21e05737cbed223881761bce5fffaa41e7a2
Dalton Reis <dalton.reis@gmail.com>
2015-05-12 20:49:32 -0300

    Adicionei a plataforma iOS ao projeto.

---
+++ PhoneGap/tagarela/platforms/ios/www/js/usar-plano.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function() {
+
+	// Carrega as pranchas vinculadas ao plano
+	var pranchas;
+	if (localStorage.plano == "img/plano1.png")
+		pranchas = new Array("img/prancha1.png", "img/prancha2.png");
+	if (localStorage.plano == "img/plano2.png")
+		pranchas = new Array("img/prancha3.png");
+	for	(var i = 0; i < pranchas.length; i++) {
+		$(".pranchas").append("<li><a href='usar-prancha.html'><img src='"+pranchas[i]+"' alt='' class='img-prancha' /></a></li>");    	
+	}
+	
+	$(".img-prancha").mouseover(function() {
+  		localStorage.prancha = $(this).attr("src");
+	});
+		
+});
---
+++ PhoneGap/tagarela/platforms/ios/www/js/usar-prancha.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function() {
+
+	// Carrega os simbolos vinculados a prancha
+	var simbolos;
+	var audios;
+	if (localStorage.prancha == "img/prancha1.png") {
+		simbolos = new Array("img/no-symbol.png", "img/no-symbol.png", "img/no-symbol.png",
+							 "img/pessoas-amarelo/giovanna-paciente.png", "img/verbos-verde/beber.png", "img/substantivos-laranja/suco.png",
+							 "img/no-symbol.png", "img/no-symbol.png", "img/no-symbol.png");
+		audios = new Array("no-sound", "no-sound", "no-sound",
+						   "giovana", "beber", "suco",
+						   "no-sound", "no-sound", "no-sound");
+	}
+	if (localStorage.prancha == "img/prancha2.png") {
+		simbolos = new Array("img/no-symbol.png", "img/no-symbol.png", "img/no-symbol.png",
+							 "img/pessoas-amarelo/giovanna-paciente.png", "img/verbos-verde/andar.png", "img/no-symbol.png",
+							 "img/no-symbol.png", "img/no-symbol.png", "img/no-symbol.png");
+		audios = new Array("no-sound", "no-sound", "no-sound",
+						   "giovana", "andar", "no-sound",
+						   "no-sound", "no-sound", "no-sound");
+	}
+	if (localStorage.prancha == "img/prancha3.png") {
+		simbolos = new Array("img/no-symbol.png", "img/no-symbol.png", "img/no-symbol.png",
+							 "img/pessoas-amarelo/gabriel-irmao.png", "img/verbos-verde/eu-quero-assistir.png", "img/substantivos-laranja/cocorico.png",
+							 "img/no-symbol.png", "img/no-symbol.png", "img/no-symbol.png");
+		audios = new Array("no-sound", "no-sound", "no-sound",
+						   "gabriel", "assistir", "fantoches",
+						   "no-sound", "no-sound", "no-sound");
+	}
+	for	(var i = 0; i < simbolos.length; i++) {
+		$(".simbolos").append("<li><img src='"+simbolos[i]+"' alt='"+audios[i]+"' class='img-simbolo play' /></a></li>");
+	}
+	
+	var alt;
+	$(".img-simbolo").mouseover(function() {
+  	 	alt = $(this).attr("alt");
+  		localStorage.audio = "audio/"+alt+".mp3";
+	});
+
+});
