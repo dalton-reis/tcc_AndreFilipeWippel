f7a588f6fa14f1b084f471d54513c0ab1f023c07
afwippel <wippelandre@hotmail.com>
2015-09-02 22:01:11 -0300

    materialize (material design css)

--- PhoneGap/tagarela/www/js/_vti_cnf/index.js
+++ PhoneGap/tagarela/www/js/_vti_cnf/index.js
@@ -4,7 +4,7 @@ vti_modifiedby:SR|VAIO\\Usuario
 vti_timelastmodified:TR|06 May 2015 22:24:56 -0000
 vti_timecreated:TR|03 May 2015 14:30:56 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|index.html
+vti_backlinkinfo:VX|index2.html index.html
 vti_nexttolasttimemodified:TW|06 May 2015 22:24:41 -0000
 vti_cacheddtm:TX|06 May 2015 22:24:56 -0000
 vti_filesize:IR|1045
