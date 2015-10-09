f6d5c219e3385978b45c640dba6dd09b2c566f61
afwippel <wippelandre@hotmail.com>
2015-10-09 19:50:40 -0300

    correção do "path para BD no OpenShift"

--- PhoneGap/tagarela/www/scripts/home-modal.php
+++ PhoneGap/tagarela/www/scripts/home-modal.php
@@ -11,7 +11,7 @@
     $retEmail = "";
     $retTel = "";
     
-	$con = mysqli_connect("localhost","root","","tagarela_bd");
+	$con = mysqli_connect("127.6.181.2","adminHh2zviV","q7DGyPAINIsG","tagarela_bd","3306");
 	if (!$con) {
 		$erro = true;
 		$msg = "Não foi possível conectar no banco de dados! Erro: " . mysqli_connect_error();
